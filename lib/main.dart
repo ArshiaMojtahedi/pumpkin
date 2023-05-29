7c6871316ff90744dd0f74f50f381b34af52a995
Arshia Mojtahedi <arshia.mojtahedi@zamann-pharma.com>
2023-05-30 02:23:40 +0330

    removed flutter init to check web build.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'Data/local/hive_client.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initModule();
-  await initHive();
+  // await initHive();
   runApp(const MyApp());
 }
 
